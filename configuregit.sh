78a2f12c98b45a74d725f01fc3fa37570c946b9e
MAQ_PCUbuntu <19463568+peret2000@users.noreply.github.com>
2023-09-01 20:51:54 +0200

    Corrected: aliases are moved to .bashrc instead of the script
    In case on a container, if the script is executed from the Dockerfile, aliases would not take effect in yhe container

--- configuregit.sh
+++ configuregit.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-git config alias.lld 'log --pretty=format:"%C(yellow)%h%x20%C(cyan)%d%x20%C(green)%ad%x20%C(reset)%s%x20%C(red)(%an)" --graph --all --date=relative'
-git config alias.ll 'log  --decorate --all --graph --oneline'
-git config alias.lldo 'log --pretty=format:\"%C(yellow)%h%x20%C(cyan)%d%x20%C(green)%ad%x20%C(reset)%s%x20%C(red)(%an)\" --graph --date=relative'
-
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR
 
