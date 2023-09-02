6bbdfbba694e7ef77cdca11fe2c884d76a538242
MAQ_PCUbuntu <19463568+peret2000@users.noreply.github.com>
2023-09-02 11:35:46 +0200

    Reverted 78a2f12. Aliases again in script instead of .bashrc
    Using --global flag for 'git alias' adds aliases to .gitconfig file

--- configuregit.sh
+++ configuregit.sh
@@ -3,6 +3,10 @@
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR
 
+git config --global alias.lld 'log --pretty=format:"%C(yellow)%h%x20%C(cyan)%d%x20%C(green)%ad%x20%C(reset)%s%x20%C(red)(%an)" --graph --all --date=relative'
+git config --global alias.ll 'log  --decorate --all --graph --oneline'
+git config --global alias.lldo 'log --pretty=format:\"%C(yellow)%h%x20%C(cyan)%d%x20%C(green)%ad%x20%C(reset)%s%x20%C(red)(%an)\" --graph --date=relative'
+
 
 cp git-* $HOME
 cat bashrc4git >> $HOME/.bashrc
