9fcd85fac38ae5a320f3dca88c3ed3736acdd623
Peret <19463568+peret2000@users.noreply.github.com>
2024-01-12 09:34:30 +0100

    Update configuregit.sh. corrected alias 'lldo'

--- configuregit.sh
+++ configuregit.sh
@@ -15,7 +15,7 @@ cd $SCRIPT_DIR
 
 git config --global alias.lld 'log --pretty=format:"%C(yellow)%h%x20%C(cyan)%d%x20%C(green)%ad%x20%C(reset)%s%x20%C(red)(%an)" --graph --all --date=relative'
 git config --global alias.ll 'log  --decorate --all --graph --oneline'
-git config --global alias.lldo 'log --pretty=format:\"%C(yellow)%h%x20%C(cyan)%d%x20%C(green)%ad%x20%C(reset)%s%x20%C(red)(%an)\" --graph --date=relative'
+git config --global alias.lldo 'log --pretty=format:"%C(yellow)%h%x20%C(cyan)%d%x20%C(green)%ad%x20%C(reset)%s%x20%C(red)(%an)" --graph --date=relative'
 
 if [ "$#" -eq 2 ]; then
 	git config --global user.name "$1"
